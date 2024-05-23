cac5fef7461f15290a0fb0d44a68a86619a3b94c
Developer Williams <archywilliams2@gmail.com>
2024-05-23 21:00:26 +0300

    Added CREATE Functionality

--- client/src/UserData.jsx
+++ client/src/UserData.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
@@ -7,6 +6,12 @@ const UserData = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [show, setShow] = useState(false);
+  const [formData, setForm] = useState({
+    Username: "",
+    Email: "",
+    Password: "",
+  });
 
   useEffect(() => {
     const displayUsers = async () => {
@@ -34,30 +39,81 @@ const UserData = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>There was an Error</p>;
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8080/users", formData);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setForm({ ...formData, [e.target.name]: value });
+  };
+
   return (
-    <>
-      <div className="home">
-        <h1>User List</h1>
-        {users.map((user) => (
-          <ul key={user._id}>
-            <li>
-              {user.Username} - {user.Email}
-              <div>
-                <button className="edit">Edit</button>
-                <button
-                  className="del"
-                  onClick={() => {
-                    handleDelete(user._id);
-                  }}
-                >
-                  Delete
-                </button>
-              </div>
-            </li>
-          </ul>
-        ))}
-      </div>
-    </>
+    <div className="home">
+      <h1>User List</h1>
+      <button
+        className="add"
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        Add User
+      </button>
+      {users.map((user) => (
+        <ul key={user._id}>
+          <li>
+            <h3>Name</h3> {user.Username} <h3>Email</h3> {user.Email}
+            <div>
+              <button className="edit">Edit</button>
+              <button
+                className="del"
+                onClick={() => {
+                  handleDelete(user._id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        </ul>
+      ))}
+      {show && (
+        <div className="myform">
+          <form className="form" onSubmit={handleSubmit}>
+            <h3>Add User</h3>
+            <div>
+              <input
+                type="text"
+                placeholder="Your Email Address"
+                name="Username"
+                value={formData.Username}
+                onChange={handleChange}
+              />
+              <input
+                type="email"
+                placeholder="Your Email Address"
+                name="Email"
+                value={formData.Email}
+                onChange={handleChange}
+              />
+              <input
+                type="Password"
+                value={formData.Password}
+                placeholder="Your Password"
+                name="Password"
+                onChange={handleChange}
+              />
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      )}
+    </div>
   );
 };
 
